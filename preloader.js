0bc235ec457ae398ec35ffe828bb56470e54ac1b
Maks <45541229+TheMafable@users.noreply.github.com>
2018-12-02 20:39:22 +0200

    Files for site

---
+++ preloader.js
@@ -0,0 +1,38 @@
+    var
+        images              = document.images,
+        images_total_count  = images.length,
+        images_loaded_count = 0,
+        perc_display = document.getElementById('perc');
+
+for (var i = 0; i < images_total_count; i++) {
+    image_clone = new Image();
+    image_clone.onload = image_loaded;
+    image_clone.onerror = image_loaded;
+    image_clone.src = images[i].src;
+}
+console.log(perc_display)
+console.log(images_total_count);
+console.log(images_loaded_count);
+
+function image_loaded() {
+ images_loaded_count++;
+perc_display.innerHTML = ( (100 / images_total_count * images_loaded_count) << 0) + '%';
+
+
+
+if (images_loaded_count >= images_total_count) {
+    document.body.onload = function() {
+    setTimeout(function() {
+        var preloader = document.getElementById('page-preloader');
+        if ( !preloader.classList.contains('done') ) 
+        {
+            preloader.classList.add('done');
+        }
+    }, 0 );
+}
+}
+}
+
+
+
+
